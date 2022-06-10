a20008fb7d45c837989016cc0223c09412a9b791
Hossein Shahmohammadi <imhshahdi@gmail.com>
2022-06-11 03:07:54 +0430

    some edit on code

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecting elements // code 4 & 5 similar
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
