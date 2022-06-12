476a9359b25482896d1702e639e6a7fb527e3fee
Hossein Shahmohammadi <imhshahdi@gmail.com>
2022-06-12 16:00:13 +0430

    Edit some code

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements // code 4 & 5 similar
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const currentScore0El = document.getElementById('current--0');
@@ -11,33 +13,75 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const playerActive = document.querySelector('.player--active');
+let scores, currentScore, activePlayer, playing;
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    currentScore0El.textContent = 0;
+    currentScore1El.textContent = 0;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+
+init();
+
+const swichPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
 
 // Rolling dice Functionality
 btnRoll.addEventListener('click', function () {
-    // 1. Generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
+    if (playing) {
+        // 1. Generating a random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
 
-    // 2. Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+        // 2. Display dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
 
-    // 3. Cheak for rolled 1
-    if (dice !== 1) {
-        // Add dice to current score
-        currentScore += dice;
-        currentScore0El.textContent = currentScore;
-    } else {
-
+        // 3. Cheak for rolled 1
+        if (dice !== 1) {
+            // Add dice to current score
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
+            swichPlayer();
+        }
     }
 });
+
+btnHold.addEventListener('click', function () {
+    if (playing) {
+        // 1. Add current score to active player's score
+        scores[activePlayer] += currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+        // 2. Check if player's score is >= 100  // Finish Game
+        if (scores[activePlayer] >= 20) {
+            playing = false;
+            diceEl.classList.add('hidden');
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        } else {
+            // Swich to next player
+            swichPlayer();
+        }
+    }
+});
+
+btnNew.addEventListener('click', init);
